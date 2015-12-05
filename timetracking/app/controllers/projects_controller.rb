396f581d8259eb4ee028748684d2e6f237938bcf
Vicuko <victorc.fernandez@gmail.com>
2015-12-05 13:07:14 +0100

    Create projects index and form

--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -4,5 +4,13 @@ class ProjectsController < ApplicationController
 		@projects = Project.last_created_projects(2)
 	end
 
+	def new
+		@project = Project.new
+	end
 
+	def create
+		p = Project.new(name: params[:project][:name], description: params[:project][:description])
+		p.save
+		redirect_to '/projects'
+	end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'site#home'
   get '/contact'=> 'site#contact'
   get '/projects' => 'projects#index'
+  get '/projects/new' => 'projects#new'
+  post '/projects' => 'projects#create'
 end
