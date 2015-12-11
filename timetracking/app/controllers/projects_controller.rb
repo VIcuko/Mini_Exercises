872274683e68c4367426ba1042ff5a4eaa20a543
Vicuko <victorc.fernandez@gmail.com>
2015-12-11 19:19:28 +0100

    Update and delete projects

--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 	
 	def index
-		@projects = Project.last_created_projects(2)
+		@projects = Project.last_created_projects(30)
 	end
 
 	def new
@@ -20,5 +20,24 @@ class ProjectsController < ApplicationController
 		render 'no_hay_nada'
 	end
 
+	def edit
+		@project=Project.find(params[:id])
+	end
 
+	def update
+		new_project = params[:project]
+		name = new_project[:name]
+		description = new_project[:description]
+				
+		@project=Project.find(params[:id])
+		@project.update_attributes(name: name, description: description)
+		redirect_to '/projects'
+	end
+
+	def destroy
+		project=Project.find(params[:id])
+		project.destroy
+
+		redirect_to '/projects'
+	end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'site#home'
-  get '/contact'=> 'site#contact'
-  get '/projects' => 'projects#index'
-  get '/projects/new' => 'projects#new'
-  post '/projects' => 'projects#create'
-  get '/projects/:id' => 'projects#show'
+  get 'contact'=> 'site#contact'
+  get 'projects' => 'projects#index'
+  get 'projects/new' => 'projects#new'
+  post 'projects' => 'projects#create'
+  get 'projects/:id' => 'projects#show', as: 'project'
+  get 'projects/:id/edit' => 'projects#edit'
+  patch 'projects/:id' => 'projects#update'
+  delete 'projects/:id' => 'projects#destroy'
 end
