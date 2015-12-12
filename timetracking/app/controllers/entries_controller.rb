9bdb7b0f2f78d81687a735f14c7f58a84ec6062c
Vicuko <victorc.fernandez@gmail.com>
2015-12-12 12:30:25 +0100

    Create errors in form screen

--- timetracking/app/controllers/entries_controller.rb
+++ timetracking/app/controllers/entries_controller.rb
@@ -13,6 +13,19 @@ class EntriesController < ApplicationController
 	end
 
 	def create
-		binding.pry
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.new(entry_params)
+		if @entry.save
+			redirect_to "/projects/#{@project.id}/entries"
+		else
+			render "new"
+		end
+		
+		
+	end
+	
+	private
+	def entry_params
+		params.require(:entry).permit(:hours, :minutes, :date)
 	end
 end
--- timetracking/app/models/entry.rb
+++ timetracking/app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
 	validates :hours, numericality: true
 	validates :minutes, numericality: {only_integer: true, greater_than: 0}
-	validates :date, :hours, :minute, presence: true
+	validates :date, :hours, :minutes, presence: true
 	validates :project_id, presence: true
 
 	belongs_to :project
